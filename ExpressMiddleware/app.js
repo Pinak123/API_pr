356496ee0461c367345cb2fc03c438fa6e726c27
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2023-12-19 18:56:29 +0530

    did some work on middleware

--- ExpressMiddleware/app.js
+++ ExpressMiddleware/app.js
@@ -4,17 +4,22 @@ const logger = require('./logger');
 
 // request ==> middleware ==> response
 //................->logger function moved to logger.js<-...........
+app.use(logger);// --> instead of putting logger in app.get of all routs
 
-
-app.get('/',logger , (req, res) => {
+app.get('/' , (req, res) => {
     res.send("Home page");
 
 })
 
 
-app.get('/about',logger, (req, res) => {
+app.get('/about', (req, res) => {
     res.send("about");
 });
+
+app.get('/contents', (req, res) => {
+    res.send("contents");
+});
+
 // console.log(logger,"hello");
 
 app.listen(3000, ()=>console.log('listening on port 3000'));
