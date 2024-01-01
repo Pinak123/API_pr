9e46dbba211d455da25618db68fa5b4404734e3f
Pinak123 <77087801+Pinak123@users.noreply.github.com>
2024-01-01 18:47:44 +0530

    POST method completed

--- http_methods/app.js
+++ http_methods/app.js
@@ -5,13 +5,24 @@ const {people} = require('./data');
 
 
 app.use(express.static('./methods-public'));
+//parse form data
 app.use(express.urlencoded({extended: false}));
-
+//parse incomming data
+app.use(express.json());
 
 
 app.get('/api/people' , (req, res) => {
     res.status(200).json({succes:true , data:people});
 });
+
+app.post('/api/people', (req, res) => {
+    const {name} = req.body;
+    if (!name) return res.status(400).json({succes:false , msg: 'Invalid name'});
+    return res.status(201).json({succes:true , person:name});
+});
+
+
+
 
 app.post('/login', (req, res) => {
     const {name}=req.body;
